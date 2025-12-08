ad174f6a41ef525a3de00681bb13ce2d4e5ec55b
rajathjn <rajathjnx@gmail.com>
2025-12-09 01:04:14 +0530

    Remove redundant blank line in main.go

--- cmd/frostmourne_tunes/main.go
+++ cmd/frostmourne_tunes/main.go
@@ -1,42 +1,51 @@
 package main
 
 import (
-	"log"
-	"os"
 	"github.com/bwmarrin/discordgo"
 	"github.com/rajathjn/frostmourne_tunes/internal/utils"
+	"log"
+	"os"
 )
 
 // load .env file in configs folder to get DISCORD_TOKEN
 const ENV_FILE string = "configs/.env"
 
 func main() {
-    err := utils.LoadEnvFile(ENV_FILE)
-    if err != nil {
-        log.Fatalf("Error loading .env file: %v", err)
-        return
-    }
+	err := utils.LoadEnvFile(ENV_FILE)
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+		return
+	}
 
-    DISCORD_TOKEN := os.Getenv("DISCORD_TOKEN")
+	DISCORD_TOKEN := os.Getenv("DISCORD_TOKEN")
 
-    // Create a new Discordgo session
+	// Create a new Discordgo session
 	music_bot, err := discordgo.New("Bot " + DISCORD_TOKEN)
 	if err != nil {
 		log.Fatalf("Error creating Discord session: %v", err)
 		return
 	}
 
-    log.Println("Welcome to Frostmourne Tunes!")
+	log.Println("Welcome to Frostmourne Tunes!")
 
-    // Hello World message sent by bot
-    music_bot.Open()
-    defer music_bot.Close()
+	// Hello World message sent by bot
+	err = music_bot.Open()
+	if err != nil {
+		log.Fatalf("Error opening Discord session: %v", err)
+		return
+	}
 
-    _, err = music_bot.ChannelMessageSend("1081885596227739692", "Hello World!")
-    if err != nil {
-        log.Fatalf("Error sending message: %v", err)
-        return
-    }
-    
+	defer func() {
+		err := music_bot.Close()
+		if err != nil {
+			log.Fatalf("Error closing Discord session: %v", err)
+		}
+	}()
+
+	_, err = music_bot.ChannelMessageSend("1081885596227739692", "Hello World!")
+	if err != nil {
+		log.Fatalf("Error sending message: %v", err)
+		return
+	}
 
 }
